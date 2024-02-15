fb5cd5984105323af67365256275fb7d3b4407a8
Kin Ishii <kinishii97@gmail.com>
2024-02-16 06:42:08 +0900

    criando a base de dados tabelas e populando com dados iniciais

---
+++ src/02_challenge.sql
@@ -0,0 +1,43 @@
+USE Trybefy;
+CREATE TABLE plans (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    price NUMERIC(5, 2) NOT NULL
+);
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    full_name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    birthday DATE,
+    active BOOLEAN DEFAULT true,
+    plan_id INTEGER NOT NULL,
+    FOREIGN KEY (plan_id) REFERENCES plans(id)
+);
+
+INSERT INTO plans (name, price) 
+VALUES ('Anual', 39.90), ('Mensal', 5.90), ('Trimestral', 19.90);
+
+
+id 	full_name 	email 	birthday 	active 	plan_id
+1 	Pedro 	pedro@trybefy.com 	1990-01-01 	true 	1
+2 	Camila 	camila@trybefy.com 	1988-12-01 	true 	1
+3 	Guilherme 	guilherme@trybefy.com 	1988-12-01 	true 	2
+4 	Andressa 	andressa@trybefy.com 	1984-07-20 	false 	3
+5 	Luís 	luis@trybefy.com 	2000-01-01 	true 	2
+6 	Cássia 	cassia@trybefy.com 	1995-12-01 	true 	1
+7 	Simone 	simone@trybefy.com 	1988-12-01 	true 	1
+8 	Rogério 	rogerio@trybefy.com 	1979-12-01 	true 	3
+9 	Júlio 	julio@trybefy.com 	1994-06-20 	false 	2
+10 	Melissa 	melissa@trybefy.com 	1997-04-11 	true 	2
+
+INSERT INTO users (full_name, email, birthday, active, plan_id)
+VALUES ('Pedro', 'pedro@trybefy.com', '1990-01-01', true, 1),
+('Camila', 'camila@trybefy.com', '1988-12-01', true, 1),
+('Guilherme', 'guilherme@trybefy.com', '1988-12-01', true, 2),
+('Andressa', 'andressa@trybefy.com', '1984-07-20', false, 3),
+('Luís', 'luis@trybefy.com', '2000-01-01', true, 2),
+('Cássia', 'cassia@trybefy.com', '1995-12-01', true, 1),
+('Simone', 'simone@trybefy.com', '1988-12-01', true, 1),
+('Rogério', 'rogerio@trybefy.com', '1979-12-01', true, 3),
+('Júlio', 'julio@trybefy.com', '1994-06-20', false, 2),
+('Melissa', 'melissa@trybefy.com', '1997-04-11', true, 2);
